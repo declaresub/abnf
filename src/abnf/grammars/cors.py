dcc8584443d0350527283aa08f68415e55e24c3e
Charles Yeomans <charles@charles.local>
2018-07-11 16:41:39 -0400

    Add grammar for cors headers.

---
+++ src/abnf/grammars/cors.py
@@ -0,0 +1,37 @@
+"""
+Collected rules from fetch standard
+https://fetch.spec.whatwg.org/#cors-protocol
+"""
+
+from ..parser import Rule as _Rule
+from . import rfc3986, rfc7230, rfc7234
+from .misc import load_grammar_rules
+
+
+
+@load_grammar_rules([
+    ('scheme', rfc3986.Rule('scheme')),
+    ('host', rfc3986.Rule('host')),
+    ('port', rfc3986.Rule('port')),
+    ('method', rfc7230.Rule('method')),
+    ('field-name', rfc7230.Rule('field-name')),
+    ('delta-seconds', rfc7234.Rule('delta-seconds')),
+    ])
+class Rule(_Rule):
+    """Rules from RFC 8187."""
+
+    grammar = [
+    'Origin = origin-or-null'
+    'origin-or-null = origin / %x6E.75.6C.6C',
+    'origin = scheme "://" host [ ":" port ]'
+    'Access-Control-Request-Method = method,
+    'Access-Control-Request-Headers = 1#field-name,
+
+    'wildcard = "*",
+    'Access-Control-Allow-Origin = origin-or-null / wildcard',
+    'Access-Control-Allow-Credentials = %x74.72.75.65',
+    'Access-Control-Expose-Headers = #field-name',
+    'Access-Control-Max-Age = delta-seconds,
+    'Access-Control-Allow-Methods = #method,
+    'Access-Control-Allow-Headers = #field-name,
+    ]
