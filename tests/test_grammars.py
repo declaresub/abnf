a844885e2920ffa76d41ce9a463f44bae89d337a
Charles Yeomans <charles@charles.local>
2020-04-06 11:08:56 -0400

    Add cors to list of grammar modules tested.

--- tests/test_grammars.py
+++ tests/test_grammars.py
@@ -3,7 +3,7 @@ import abnf.grammars
 import pkgutil
 from importlib import import_module
 
-@pytest.mark.parametrize("rfc", map(import_module, ['%s.%s' % (abnf.grammars.__name__, x[1]) for x in pkgutil.walk_packages(abnf.grammars.__path__) if x[1].startswith('rfc')]))
+@pytest.mark.parametrize("rfc", map(import_module, ['%s.%s' % (abnf.grammars.__name__, x[1]) for x in pkgutil.walk_packages(abnf.grammars.__path__) if x[1] == 'cors' or x[1].startswith('rfc')]))
 def test_grammar(rfc):
     """Catches rules used but not defined in grammar."""
     for rule in rfc.Rule.rules():
