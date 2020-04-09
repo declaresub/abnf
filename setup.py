b227db676c62d0d4dee9680cd2a1a103a66c43c0
Charles Yeomans <charles@charles.local>
2020-04-09 19:35:29 -0400

    Some package metadata is relocated to __init__.py.

--- setup.py
+++ setup.py
@@ -9,25 +9,25 @@ import os
 
 PACKAGE_NAME = 'abnf'
 
-def package_version(pkg_name=PACKAGE_NAME):
+def meta(key, pkg_name=PACKAGE_NAME):
     with io.open(os.path.join('src', pkg_name, '__init__.py'), 'r', encoding='utf-8') as f:
         for sourceline in f:
-            if sourceline.strip().startswith('__version__'):
+            if sourceline.strip().startswith('__%s__' % key):
                 return sourceline.split('=',
                                         1)[1].strip(string.whitespace + '"\'')
         else:
-            raise Exception('Unable to read package version.')
+            raise Exception('Unable to find metadata key %s.', key)
 
 
 setup(
     name=PACKAGE_NAME,
-    version=package_version(),
+    version=meta('version'),
     description='Parsers for ABNF grammars.',
     long_description=
     'ABNF generates parsers for ABNF grammars. Though intended for use with \
     RFC grammars, ABNF should handle any valid grammar.',
-    author='Charles Yeomans',
-    author_email='charles@declaresub.com',
+    author=meta('author'),
+    author_email=meta('author_email'),
     url='https://bitbucket.org/yeomans/abnf',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
@@ -43,7 +43,8 @@ setup(
         'Programming Language :: Python :: 3.7'
         'Programming Language :: Python :: 3.8'
     ],
-    license='MIT',
+    license=meta('license'),
     python_requires='>=3',
     install_requires=[],
-    zip_safe=False)
+    zip_safe=False,
+    options={"bdist_wheel": {"universal": "1"}})
--- src/abnf/__init__.py
+++ src/abnf/__init__.py
@@ -1,5 +1,11 @@
 """Parser generator for ABNF grammars."""
 
-__version__ = "1.0.0"
-
 from .parser import Rule, Node, LiteralNode, NodeVisitor, ParseError, GrammarError
+
+
+__version__ = "1.0.0"
+__author__ = "Charles Yeomans"
+__author_email__ = "charles@declaresub.com"
+__project_url__ = 'https://bitbucket.org/yeomans/abnf'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c)2020 Charles Yeomans'
