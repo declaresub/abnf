864adc1848e5bdb0af78da6e76aad3704bdc9b7a
declaresub <charles@declaresub.com>
2020-04-10 18:50:59 -0400

    Switch __project_url__ to github.

--- src/abnf/__init__.py
+++ src/abnf/__init__.py
@@ -6,6 +6,6 @@ from .parser import Rule, Node, LiteralNode, NodeVisitor, ParseError, GrammarErr
 __version__ = "1.0.0"
 __author__ = "Charles Yeomans"
 __author_email__ = "charles@declaresub.com"
-__project_url__ = "https://bitbucket.org/yeomans/abnf"
+__project_url__ = "https://github.com/declaresub/abnf"
 __license__ = "MIT"
 __copyright__ = "Copyright (c)2020 Charles Yeomans"
