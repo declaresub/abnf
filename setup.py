6f55b0dd76a2444b05125585e8e514917cb3a346
declaresub <charles@declaresub.com>
2020-04-15 10:39:42 -0400

    Revise classifiers.

--- setup.py
+++ setup.py
@@ -33,9 +33,11 @@ setup(
     package_dir={"": "src"},
     keywords=['abnf', 'parser', 'generator'],
     classifiers=[
-        'Development Status :: 4 - Beta', 'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Interpreters''
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
