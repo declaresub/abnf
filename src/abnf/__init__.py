deb22879465629315a9c6f6cb0ef64788d899a2d
declaresub <charles@declaresub.com>
2020-05-11 18:20:17 -0400

    Bump __version__ to 1.0.1.

--- src/abnf/__init__.py
+++ src/abnf/__init__.py
@@ -3,7 +3,7 @@
 from .parser import Rule, Node, LiteralNode, NodeVisitor, ParseError, GrammarError
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Charles Yeomans"
 __author_email__ = "charles@declaresub.com"
 __project_url__ = "https://github.com/declaresub/abnf"
