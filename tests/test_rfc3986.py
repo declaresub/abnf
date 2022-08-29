f7ab18024273483ec755f0a584da779d633f77fe
declaresub <charles@declaresub.com>
2022-08-29 17:01:31 -0400

    Add more IPv6 test cases.

--- tests/test_rfc3986.py
+++ tests/test_rfc3986.py
@@ -6,6 +6,9 @@ from abnf.grammars import rfc3986
 @pytest.mark.parametrize('src, value', [
 # test added thanks to https://github.com/declaresub/abnf/issues/10.
 ('2001:0db8:0000:0000:0000:ff00:0042:8329', '2001:0db8:0000:0000:0000:ff00:0042:8329'), 
+('2001:db8:cafe::17', '2001:db8:cafe::17'),
+('0:0:0:0:0:ffff:192.1.56.10', '0:0:0:0:0:ffff:192.1.56.10'),
+('::', '::'),
 ])
 def test_IPv6address(src: str, value: str):
     ip6 = rfc3986.Rule('IPv6address')
