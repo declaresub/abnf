4916cc275551470842912343e2b6d24c66c41439
declaresub <charles@declaresub.com>
2022-09-05 12:19:35 -0400

    Add type hints.

--- tests/test_rfc7231.py
+++ tests/test_rfc7231.py
@@ -1,6 +1,7 @@
 import pytest
 
 from abnf.grammars import rfc7231
+from abnf.parser import Source
 
 @pytest.mark.parametrize("src", [
 '*/*',
@@ -8,7 +9,7 @@ from abnf.grammars import rfc7231
 'image/*',
 'text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8'
 ])
-def test_accept_parse(src):
+def test_accept_parse(src: Source):
     accept = rfc7231.Rule('accept')
     assert accept.parse_all(src)
 
@@ -18,6 +19,6 @@ def test_accept_parse(src):
 'Tuesday, 08-Feb-94 14:15:29 GMT',
 'Thu Feb  3 00:00:00 1994',
 ])
-def test_date_parse(src: str):
+def test_date_parse(src: Source):
     date = rfc7231.Rule("Date")
     assert date.parse_all(src)
