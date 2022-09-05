bdca29e74f32e1e977c4ce33aabbc2ea6b5f386a
declaresub <charles@declaresub.com>
2022-09-05 12:20:57 -0400

    Add type hint.

--- tests/test_grammars.py
+++ tests/test_grammars.py
@@ -2,9 +2,10 @@ import pytest
 import abnf.grammars
 import pkgutil
 from importlib import import_module
+import types
 
 @pytest.mark.parametrize("rfc", map(import_module, ['%s.%s' % (abnf.grammars.__name__, x[1]) for x in pkgutil.walk_packages(abnf.grammars.__path__) if x[1] == 'cors' or x[1].startswith('rfc')]))
-def test_grammar(rfc):
+def test_grammar(rfc: types.ModuleType):
     """Catches rules used but not defined in grammar."""
     for rule in rfc.Rule.rules():
         if not hasattr(rule, 'definition'):
