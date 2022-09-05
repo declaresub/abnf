66bc33e5a8da1d2a93432d44e056a01a46f936ee
declaresub <charles@declaresub.com>
2022-09-05 12:26:09 -0400

    Add type hints.

--- tests/fuzz/test_rfc5234.py
+++ tests/fuzz/test_rfc5234.py
@@ -1,16 +1,17 @@
 import pytest
 import os
 import io
+import typing
 
-from abnf.parser import ABNFGrammarRule, ParseError
+from abnf.parser import ABNFGrammarRule, Source
 
 
 # fuzz test data generated using abnfgen <http://www.quut.com/abnfgen/>.
 
 FUZZ_DIR = 'tests/fuzz'
 
-def load_fuzz_test_data(dirname):
-    test_data = []
+def load_fuzz_test_data(dirname: str):
+    test_data: typing.List[str] = []
     dir = os.path.join(FUZZ_DIR, dirname)
     for filename in os.listdir(dir):
         with io.open(os.path.join(dir, filename), 'rb') as f:
@@ -19,38 +20,38 @@ def load_fuzz_test_data(dirname):
     return test_data
 
 @pytest.mark.parametrize("src", load_fuzz_test_data('char-val'))
-def test_char_val(src):
+def test_char_val(src: Source):
     node, _ = ABNFGrammarRule('char-val').parse(src, 0)
     assert node and node.value == src
 
 @pytest.mark.parametrize("src", load_fuzz_test_data('num-val'))    
-def test_num_val(src):
+def test_num_val(src: Source):
     node, _ = ABNFGrammarRule('num-val').parse(src, 0)
     assert node and node.value == src
 
 @pytest.mark.parametrize("src", load_fuzz_test_data('repeat'))
-def test_repeat(src):
+def test_repeat(src: Source):
     node, _ = ABNFGrammarRule('repeat').parse(src, 0)
     assert node and node.value == src
 
 @pytest.mark.parametrize("src", load_fuzz_test_data('comment'))
-def test_comment(src):
+def test_comment(src: Source):
     node, _ = ABNFGrammarRule('comment').parse(src, 0)
     assert node and node.value == src
 
 @pytest.mark.parametrize("src", [
 """;foo\r\n""",
 """\r\n"""])
-def test_c_nl(src):
+def test_c_nl(src: Source):
     node, _ = ABNFGrammarRule('c-nl').parse(src, 0)
     assert node and node.value == src
 
 @pytest.mark.parametrize("src", load_fuzz_test_data('c-wsp'))
-def test_c_wsp(src):
+def test_c_wsp(src: Source):
     node, _ = ABNFGrammarRule('c-wsp').parse(src, 0)
     assert node and node.value == src
 
 @pytest.mark.parametrize("src", load_fuzz_test_data('rule'))
-def test_rule(src):
+def test_rule(src: Source):
     node, _ = ABNFGrammarRule('rule').parse(src, 0)
     assert node and node.value == src
