bf928113ed635caf43a029c2cb960b0553fb3e7e
declaresub <charles@declaresub.com>
2022-11-25 10:24:40 -0500

    Modify grammar following an erratum to RFC 6266 to remove an ambiguity in the grammar.
    Resolves issue #16.

--- tests/test_rfc6266.py
+++ tests/test_rfc6266.py
@@ -2,21 +2,32 @@ import pytest
 
 from abnf.grammars import rfc6266
 
-def test_ext_token():
-    ext_token = rfc6266.Rule('ext-token')
-    node = ext_token.parse_all("token*")
-    assert node.value == "token*"
 
-
-@pytest.mark.parametrize('src', [
-'Content-Disposition: inline',
-'Content-Disposition: attachment',
-'Content-Disposition: attachment; filename="filename.jpg"',
-'Content-Disposition: form-data; name="fieldName"',
-'Content-Disposition: form-data; name="fieldName"; filename="filename.jpg"',
-'Content-Disposition: attachment; filename*=UTF-8\'\'%e2%82%ac%20rates',
-'Content-Disposition: attachment; filename="EURO rates"; filename*=utf-8\'\'%e2%82%ac%20rates',
-])
+@pytest.mark.parametrize(
+    "src",
+    [
+        "Content-Disposition: inline",
+        "Content-Disposition: attachment",
+        'Content-Disposition: attachment; filename="filename.jpg"',
+        'Content-Disposition: form-data; name="fieldName"',
+        'Content-Disposition: form-data; name="fieldName"; filename="filename.jpg"',
+        "Content-Disposition: attachment; filename*=UTF-8''%e2%82%ac%20rates",
+        "Content-Disposition: attachment; filename=\"EURO rates\"; filename*=utf-8''%e2%82%ac%20rates",
+    ],
+)
 def test_content_disposition(src: str):
-    content_disposition = rfc6266.Rule('content-disposition')
+    content_disposition = rfc6266.Rule("content-disposition")
     assert content_disposition.parse_all(src)
+
+
+def test_disp_ext_parm():
+    # this test checks to ensure that the RFC 6266 grammar as modified in response to erratum
+    # parses src into an ext-name = ext-value parameter.
+    src = "foo*=utf-8''%E2%88%Aar"
+    node = rfc6266.Rule("disp-ext-parm").parse_all(src)
+    assert len(node.children) == 3
+    assert node.children[0].name == "ext-parmname"
+    assert node.children[0].value == "foo*"
+    assert node.children[1].value == "="
+    assert node.children[2].name == "ext-value"
+    assert node.children[2].value == "utf-8''%E2%88%Aar"
