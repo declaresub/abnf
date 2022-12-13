9c4ab8fa0bbec2b083c15475d75e850c6425e320
declaresub <charles@declaresub.com>
2022-12-13 12:59:42 -0500

    Revert WWW-Authenticate rule tweak.
    Current parser correctly parses using the rule as defined in rc7235.

--- src/abnf/grammars/rfc7235.py
+++ src/abnf/grammars/rfc7235.py
@@ -30,14 +30,7 @@ class Rule(_Rule):
         #'Proxy-Authenticate = *( "," OWS ) challenge *( OWS "," [ OWS challenge ] )',
         'Proxy-Authenticate = *( "," OWS ) challenge *( OWS ("," / challenge) )',
         "Proxy-Authorization = credentials",
-        # The rule WWW-Authenticate is ambiguous. I am not the first person to observe this
-        # <https://www.ietf.org/mail-archive/web/httpbisa/current/msg07914.html>.
-        # The problem is essentially nested comma-separated lists.  Using the rules given
-        # in RFC 7235 with this parser, parsing source 'Basic realm="foo", Pascal realm="bar"
-        # using the rule WWW-Authenticate will result in the consumption of 'Basic realm="foo",'.
-        # So we tinker a bit to account for the challenge rule consuming the trailing comma.
-        #'WWW-Authenticate = *( "," OWS ) challenge *( OWS "," [ OWS challenge ] )',
-        'WWW-Authenticate = *( "," OWS ) challenge *( OWS ("," / challenge) )',
+        'WWW-Authenticate = *( "," OWS ) challenge *( OWS "," [ OWS challenge ] )',
         'auth-param = token BWS "=" BWS ( token / quoted-string )',
         "auth-scheme = token",
         'challenge = auth-scheme [ 1*SP ( token68 / [ ( "," / auth-param ) *(OWS "," [ OWS auth-param ] ) ] ) ]',
