85a7e0cf7818b576cc6879c48368021d22cfc481
declaresub <charles@declaresub.com>
2022-12-13 13:03:18 -0500

    Add load_grammar decorator for loading of str grammar.

--- src/abnf/grammars/misc.py
+++ src/abnf/grammars/misc.py
@@ -12,8 +12,32 @@ def load_grammar_rules(imported_rules: Optional[List[Tuple[str, Rule]]]=None):
 
     def rule_decorator(cls: Type[Rule]):
         """The function returned by decorator."""
+
+        if isinstance(cls.grammar, str):
+            raise TypeError('This decorator must be used with a grammar of tyoe list')
+
         for src in cls.grammar:
             cls.create(src)
+        if imported_rules:
+            for rule_def in imported_rules:
+                cls(rule_def[0], rule_def[1].definition)
+        return cls
+
+    return rule_decorator
+
+
+def load_grammar(imported_rules: Optional[List[Tuple[str, Rule]]]=None):
+    """A decorator that loads grammar rules following class declaration.  The code assumes
+    that cls is a Rule subclass with a grammar attribute.
+    The imported_rules parameter allows one to import rules from other modules. For examples,
+    see for instance rfc7230.py.
+    """
+
+    def rule_decorator(cls: Type[Rule]):
+        """The function returned by decorator."""
+        if isinstance(cls.grammar, list):
+            raise TypeError('This decorator must be used with a grammar of tyoe str.')
+        cls.load_grammar(cls.grammar)
         if imported_rules:
             for rule_def in imported_rules:
                 cls(rule_def[0], rule_def[1].definition)
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,23 +1,49 @@
 import typing
 
-from abnf.parser import Rule as _Rule, Literal
-from abnf.grammars.misc import load_grammar_rules
+import pytest
+
+from abnf.grammars.misc import load_grammar, load_grammar_rules
+from abnf.parser import Literal
+from abnf.parser import Rule as _Rule
 
 
 class ImportRule(_Rule):
     pass
-    
-ImportRule('test', Literal('test'))    
 
 
-@load_grammar_rules(
-[
-('test', ImportRule('test'))
-])
+ImportRule("test", Literal("test"))
+
+
+@load_grammar_rules([("test", ImportRule("test"))])
 class Rule(_Rule):
-    """Rules from RFC 5646."""
-
     grammar: typing.List[str] = []
 
+
 def test_misc_load_grammar_rules_import():
-    assert Rule('test').definition == ImportRule('test').definition
+    assert Rule("test").definition == ImportRule("test").definition
+
+
+@load_grammar([("test", ImportRule("test"))])
+class Rule1(_Rule):
+    grammar: str = ''
+
+def test_load_grammar():
+    assert Rule("test").definition == ImportRule("test").definition
+
+
+class Foo(_Rule):
+    grammar = 'foo="bar"'
+
+
+def test_load_grammar_rules_str():
+    with pytest.raises(TypeError):
+        load_grammar_rules()(Foo)
+
+
+class Bar(_Rule):
+    grammar = ['foo="bar"']
+
+
+def test_load_grammar_list():
+    with pytest.raises(TypeError):
+        load_grammar()(Bar)
