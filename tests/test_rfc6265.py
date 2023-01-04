d2af42a751dd881e60c0bb534d2e13c2c77374e7
declaresub <charles@declaresub.com>
2023-01-04 11:31:30 -0500

    Rewrite implementation of RFC 6265 to import only from earlier RFCs as specified.

--- tests/test_rfc6265.py
+++ tests/test_rfc6265.py
@@ -4,11 +4,39 @@ from abnf.grammars import rfc6265
 
 
 @pytest.mark.parametrize("src", [
-    'bar',
-    '"bar"',
-    ])
-def test_cookie_value(src: str):
-    rfc6265.Rule('cookie-value').parse_all(src)
+    "a",
+    "1",
+])
+def test_letdig(src: str):
+    node = rfc6265.LocalRule('let-dig').parse_all(src)
+    assert node.value == src
+
+
+@pytest.mark.parametrize("src", [
+    "a-",
+    "1-",
+    "a-1",
+])
+def test_ldh_str(src: str):
+    node = rfc6265.LocalRule('ldh-str').parse_all(src)
+    assert node.value == src
+
+@pytest.mark.parametrize("src", [
+    "test",
+    "a-1",
+])
+def test_label(src: str):
+    node = rfc6265.LocalRule('label').parse_all(src)
+    assert node.value == src
+
+@pytest.mark.parametrize("src", [
+    "test",
+    "example.com",
+])
+def test_subdomain(src: str):
+    node = rfc6265.LocalRule('subdomain').parse_all(src)
+    assert node.value == src
+
 
 
 @pytest.mark.parametrize("src", [
@@ -16,6 +44,6 @@ def test_cookie_value(src: str):
     "127.0.0.1",
     "FE80:CD00:0000:0CDE:1257:0000:211E:729C"
 ])
-def test_domain_value(src: str):
-    node = rfc6265.Rule('domain-value').parse_all(src)
+def _domain_value(src: str):
+    node = rfc6265.LocalRule('domain-value').parse_all(src)
     assert node.value == src
